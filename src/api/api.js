d64390ebc8aea98a430d9242c5674b0569e8d1ed
Lev <leozolotukhin@gmail.com>
2024-06-03 18:44:10 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,17 @@
 /** @format */
 import axios from "axios";
 
-const API_KEY = "mxqhNE-TNScpFD42LOXowhVB3AxvnPbafuRFLM-WRMi";
+const API_KEY = "fluBw_2FLnShYVPGvaZQIlXP-iK4rxdXZ8RRIxslK9Y";
 
-axios.defaults.baseURL = "https://api.unsplash.com/search/photos";
+axios.defaults.baseURL = "https://api.unsplash.com";
 
 export const getLink = async (query, page = 1) => {
-	const { data } = await axios.get("", {
+	const { data } = await axios.get("/search/photos", {
 		params: {
 			client_id: API_KEY,
 			query: query,
 			page: page,
 		},
 	});
-	return data.results;
+	return data.result;
 };
